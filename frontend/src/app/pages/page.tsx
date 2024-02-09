3533bccce879fb376037d751b59f13bad96cff13
gabrielszg <gabrielsouza5@hotmail.com>
2024-02-08 22:51:31 -0300

    refactor: increased font size and title spacing

--- frontend/src/app/pages/page.tsx
+++ frontend/src/app/pages/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
     return (
         <div>
             <div className="border-b-2 mb-5" >
-                <h1>Cadastro de Contato</h1>
+                <h1 className="mb-2 text-xl md:text-2xl">Cadastro de Contato</h1>
             </div>
 
             <Contacts />;
